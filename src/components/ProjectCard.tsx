bef26a5f8ad29e51afbff827df4e5ed3821cd9bf
Prabhashperera <prabashmperera01@gmail.com>
2026-01-12 14:00:00 +0530

    Content Sepration v9

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,15 +1,15 @@
-import { ImageIcon, ExternalLink, Star, GitBranch } from "lucide-react";
+import {ExternalLink, Star, GitBranch } from "lucide-react";
 import GlassCard from "./GlassCard";
 
-const ProjectCard = ({ title, description, tags, stars, forks }: any) => (
+const ProjectCard = ({ title, description, tags, stars, forks, img, link }: any) => (
     <GlassCard className="p-6 group hover:border-cyan-500/50 transition-all duration-500 flex flex-col h-full">
       <div className="h-40 mb-6 rounded-lg bg-linear-to-tr from-slate-800 to-slate-900 border border-white/5 relative overflow-hidden flex items-center justify-center text-slate-600 group-hover:text-cyan-500/50 transition-colors">
-          <ImageIcon size={32} />
+          <img src={img} alt="" />
           <div className="absolute inset-0 bg-cyan-500/10 opacity-0 group-hover:opacity-100 transition-opacity duration-500 mix-blend-overlay"></div>
       </div>
       <div className="flex justify-between items-start mb-2">
         <h3 className="text-xl font-bold text-white group-hover:text-cyan-400 transition-colors">{title}</h3>
-         <a href="#" className="text-slate-400 hover:text-cyan-400 transition-colors"><ExternalLink size={18} /></a>
+         <a href={link} target="_blank" className="text-slate-400 hover:text-cyan-400 transition-colors"><ExternalLink size={18} /></a>
       </div>
       <p className="text-slate-300 text-sm mb-4 line-clamp-3 grow">{description}</p>
       
--- src/components/ProjectsView.tsx
+++ src/components/ProjectsView.tsx
@@ -1,6 +1,11 @@
 import { motion } from "framer-motion"
 import { Terminal } from "lucide-react"
 import ProjectCard from "./ProjectCard"
+import BuildMe from '../assets/BuildMe.png'
+import FindCare from '../assets/FindCare.png'
+import ZEIDOT from '../assets/ZEIDOT.png'
+import EMP from '../assets/EMP.png'
+
 
 const ProjectsView = () => {
     return (
@@ -14,24 +19,39 @@ const ProjectsView = () => {
                 <div className="h-px bg-linear-to-r from-purple-500/50 to-transparent flex-1 ml-4"></div>
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 cursor-grab">
                 <ProjectCard 
                     title="Project ZEIDOT"
                     description="A JavaFx Management Project System to Manage And Donate Left Over Foods from Hotels to Eldery Homes, Child Homes, Homeless Peoples."
                     tags={["Java", "JavaFX", "XML", "MySQL"]}
                     stars="24" forks="8"
+                    img={ZEIDOT}
+                    link="https://github.com/Prabhashperera/Project-ZEIDOT-Layered-Architecture"
                 />
                 <ProjectCard 
                     title="FindCare WEB"
                     description="A Java EE Fullstack Network for POST found & Lost Pets, Users Can Post There Lost Pets to find, If Found Also They Can Post saying, I Found!!"
                     tags={["React JS", "SpringBoot", "Tailwind", "MySql"]}
                     stars="45" forks="12"
+                    img={FindCare}
+                    link="https://github.com/Prabhashperera/Find-Care-Full-Stack-App-Spring-Boot"
+                    
                 />
                 <ProjectCard 
                     title="BUILDME - HABIT"
                     description="Make a Good Habit in 30 Days Challenge with AI Coach and Make Fun and Gain Life Up"
                     tags={["ReactJs", "Express Js", "MongoDB", "Gemini & Groq"]}
                     stars="18" forks="4"
+                    img={BuildMe}
+                    link="https://github.com/Prabhashperera/BUILD-ME-Habit-Builder-BE"
+                />
+                <ProjectCard 
+                    title="Employee Complaint"
+                    description="Employee Complaint Management System, so Employees Can add a Complaint From there Account, Admins Can See them, and mark them"
+                    tags={["ReactJs", "Java EE", "MySql", "Google API"]}
+                    stars="1" forks="4"
+                    img={EMP}
+                    link="https://github.com/Prabhashperera/Employee-Complaint-System"
                 />
             </div>
         </motion.div>
