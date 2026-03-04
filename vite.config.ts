d790fee546c9efb3771d662230479cc2cb5348b6
Prabhashperera <prabashmperera01@gmail.com>
2026-03-05 00:14:00 +0530

    Update UI v5

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+// import { defineConfig } from 'vite'
+// import tailwindcss from '@tailwindcss/vite'
 
+// export default defineConfig({
+//   plugins: [
+//     tailwindcss(),
+//   ],
+// })
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  // I removed the "base" line from here!
 })
